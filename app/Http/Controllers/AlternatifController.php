87165e05a1d460bfae5b2af760a6bed77fab755a
shimozuki <robbishimozuki@gmail.com>
2024-07-04 08:55:22 +0800

    master

--- app/Http/Controllers/AlternatifController.php
+++ app/Http/Controllers/AlternatifController.php
@@ -28,7 +28,7 @@ class AlternatifController extends Controller
     {
         $this->validate($request, [
             'nama_alternatif' => 'required|string',
-            'nidn' => 'required|string',
+            'nidn' => 'required|string|unique:alternatif,nidn',
             'alamat' => 'required|string',
             'ttl' => 'required|string',
             'jns_klamin' => 'required|string',
@@ -41,7 +41,7 @@ class AlternatifController extends Controller
             $alternatif->nama_alternatif = $request->nama_alternatif;
             $alternatif->nidn = $request->nidn;
             $alternatif->alamat = $request->alamat;
-            $alternatif->ttl = $request->ttl; 
+            $alternatif->ttl = $request->ttl;
             $alternatif->jns_klamin = $request->jns_klamin;
             $alternatif->code_jabatan = $request->code_jabatan;
             $alternatif->telepon = $request->telepon;
@@ -50,9 +50,10 @@ class AlternatifController extends Controller
             return back()->with('msg', 'Berhasil Menambahkan Data');
         } catch (Exception $e) {
             \Log::emergency("File:" . $e->getFile() . " Line:" . $e->getLine() . " Message:" . $e->getMessage());
-            die("Gagal");
+            return back()->with('error', 'Gagal Menambahkan Data');
         }
     }
+
 
 
     public function edit($id)
@@ -85,7 +86,7 @@ class AlternatifController extends Controller
                 'nama_alternatif' => $request->nama_alternatif,
                 'nidn' => $request->nidn,
                 'alamat' => $request->alamat,
-                'ttl' => $request->ttl, 
+                'ttl' => $request->ttl,
                 'jns_klamin' => $request->jns_klamin,
                 'code_jabatan' => $request->code_jabatan,
                 'telepon' => $request->telepon,
